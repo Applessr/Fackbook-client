f9f38fdf35a1a829d086e35e2a1504666c5e9d8a
Applesst <Sasita_ssr@kkumail.com>
2024-10-01 10:16:37 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>FakeBook</h1>
+      <button className='bg-blue-300 p-2 rounded-md'>OK</button>
+    </div>
+  )
+}
+
+export default App
